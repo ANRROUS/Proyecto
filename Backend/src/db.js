dba232d0841350e7600697eb77a5b2c3bf834e76
Anrrous <arthuropipa@gmail.com>
2024-12-06 12:50:30 -0500

    Update db.js

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -6,10 +6,7 @@ dotenv.config();
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect("mongodb+srv://ANRROUS:JhQq145CSyKiLPgO@cluster0.keey7.mongodb.net/miBaseDeDatos?retryWrites=true&w=majority");
         console.log(">>>Base de datos conectada<<<");
     } catch (error) {
         console.error("Error de conexión a MongoDB:", error);
