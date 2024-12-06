bc4a41355fddd66ae22cad06666cc12d27248703
Anrrous <arthuropipa@gmail.com>
2024-12-06 12:54:09 -0500

    Update axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL:"http://localhost:3000/api",
+    baseURL:"https://proyecto-backend-a2n8.onrender.com/api",
     withCredentials:true
 })
 
